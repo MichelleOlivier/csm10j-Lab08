a497a1c6a95e2c84235adc3941aa687db6077cfc
MichelleOlivier <michelle.m.olivier@sbcglobal.net>
2015-10-20 15:18:16 -0700

    Create Car.java

---
+++ src/partA/Car.java
@@ -0,0 +1,20 @@
+//package partA;
+
+/**
+ *
+ * @author Michelle
+ */
+public class Car extends MotorVehicle
+{
+    public void TurnOn()
+    {
+        System.out.println("Car is starting");
+    }
+    
+    public void TurnOff()
+    {
+        System.out.println("Car is stopping");
+    }
+    
+    
+}
