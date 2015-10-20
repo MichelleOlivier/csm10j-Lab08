c3192fb77089b2e401e2ca6108260dae4cbd2c60
MichelleOlivier <michelle.m.olivier@sbcglobal.net>
2015-10-20 15:21:41 -0700

    Create MotorVehicle.java

---
+++ src/partB/MotorVehicle.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package partB;
+
+
+/**
+ *
+ * @author Michelle
+ */
+public abstract class MotorVehicle 
+{
+    private String name;
+    
+    public abstract void TurnOn();
+ 
+    public abstract void TurnOff();
+    
+    public String getName()
+    {
+        return name;   
+    }
+    public MotorVehicle(String _name)
+    {
+        name = _name;
+    }
+
+    
+}
